5a040583a68c52e1fd2f7a1fc44835541cc2c50d
rayetzki <rayetzkiillya@gmail.com>
2020-10-01 12:10:14 +0300

    Добавил express 101

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,12 @@
-const hello = () => 'Здарова бандит!';
-hello();
+import express, { Request, Response } from 'express';
+import morgan from 'morgan';
+
+const app = express();
+const PORT = 3000;
+
+app.use(morgan(':method :url :response-time'));
+app.use('/:id', (req: Request, res: Response) => {
+    res.send(req.params.id);
+});
+
+app.listen(PORT, () => `Listening on port ${PORT}`);
