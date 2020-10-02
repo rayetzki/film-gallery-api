97838f92d80275f88a01fee9a67a769b13693640
rayetzki <rayetzkiillya@gmail.com>
2020-10-02 09:43:11 +0300

    Вставить ссылку в базу данных

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express, { Request, Response, Express } from 'express';
 import { v2 as cloudinary } from 'cloudinary';
 import morgan from 'morgan';
 import dotenv from 'dotenv';
+import { Db } from 'mongodb';
 import bodyParser from 'body-parser';
 import MongoDB from './db';
 
@@ -22,25 +23,23 @@ app.use(morgan(':method :url :response-time'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.post(
-    '/upload-image',
-    bodyParser,
-    async (request: Request, response: Response) => {
-        try {
-            const result = await cloudinary.uploader.upload(request.body.image);
+app.post('/upload-image', async (request: Request, response: Response) => {
+    try {
+        const db: Db = request.app.get('mongo');
+        const result = await cloudinary.uploader.upload(request.body.image);
 
-            if (result) {
-                response.status(200).send({ message: 'success', result });
-            }
-        } catch (error) {
-            response.status(500).send({ message: 'Internal server error' });
+        if (result) {
+            response.status(200).send({ message: 'success', result });
+            db.collection('images').insertOne({ result });
         }
+    } catch (error) {
+        response.status(500).send({ message: 'Internal server error' });
     }
-);
+});
 
 app.listen(PORT, async () => {
-    const dbName = process.env.MONGODB_DBNAME || '';
-    const dbUrl = process.env.MONGODB_URL || '';
+    const dbName: string = process.env.MONGODB_DBNAME || '';
+    const dbUrl: string = process.env.MONGODB_URL || '';
     const db: MongoDB = new MongoDB(dbName, dbUrl);
 
     app.set('mongo', await db.connect());
