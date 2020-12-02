0340363c339ea0b733d0cb08feb29a43bcc0d15e
rayetzki <rayetzkiillya@gmail.com>
2020-12-02 14:59:48 +0200

    Added cors filters

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,10 @@ import express, { Express } from 'express';
 import { v2 as cloudinary } from 'cloudinary';
 import morgan from 'morgan';
 import dotenv from 'dotenv';
+import cors from 'cors';
 import bodyParser from 'body-parser';
 import ImagesRoutes from './routes/Images.routes';
+import { allowedNodeEnvironmentFlags } from 'process';
 
 dotenv.config();
 
@@ -20,7 +22,11 @@ app.disable('etag');
 app.use(morgan(':method :status :url :response-time'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
+app.use(
+    cors({
+        origin: ['https://film-ray.netlify.app', 'http://194.42.112.46:3000']
+    })
+);
 app.use('/api/images', ImagesRoutes);
 
 app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
