aa1502a41b789519691cfd38463aa58a6d1da021
rayetzki <rayetzkiillya@gmail.com>
2020-12-02 14:53:23 +0200

    Remove cors filters

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@ import express, { Express } from 'express';
 import { v2 as cloudinary } from 'cloudinary';
 import morgan from 'morgan';
 import dotenv from 'dotenv';
-import cors from 'cors';
 import bodyParser from 'body-parser';
 import ImagesRoutes from './routes/Images.routes';
 
@@ -18,15 +17,6 @@ const app: Express = express();
 const PORT: number = (process.env.PORT || 8080) as number;
 
 app.disable('etag');
-app.use(
-    cors({
-        origin: [
-            'http://localhost:8080',
-            'http://localhost:3000',
-            'https://rayetzki-35.netlify.app'
-        ]
-    })
-);
 app.use(morgan(':method :status :url :response-time'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
